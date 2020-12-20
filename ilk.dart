81807b5b31da40e3bf4dae90cfc50094c5d13f93
ilkeretlik <ilkeretlik@users.noreply.github.com>
2020-12-21 00:53:59 +0300

    ilk değiiklik

---
+++ ilk.dart
@@ -0,0 +1,14 @@
+import 'dart:io';
+
+main(List<String> args) {
+  print("program başladı");
+
+  Basla();
+  print("program bitti");
+}
+
+Basla() {
+  print("dosya indirme başladı......");
+  sleep(Duration(seconds: 15));
+  print("Dosya ındırme bıti");
+}
